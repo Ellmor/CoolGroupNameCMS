5edcc9e3bd92e07912507c67458881bccd196325
Piotr <nezi2007@gmail.com>
2016-03-30 15:00:46 +0200

    fix for twitter

--- server/config/config.js
+++ server/config/config.js
@@ -35,7 +35,7 @@ module.exports = {
         twitter: {
             clientID: 's5R77sfSVhyg9lnDGGDVODrh5',
             clientSecret: 'dowP4AoBt9nFPg5jvVBfZaH1IgydtQvx4P9TCJGu7LHssUjvMZ',
-            callbackURL: 'https://floating-fortress-48507.herokuapp.com:' + prodPort + '/twitter/callback'
+            callbackURL: 'https://floating-fortress-48507.herokuapp.com' + '/twitter/callback'
         },
         facebook: {
             clientID: '1087048447981846',
